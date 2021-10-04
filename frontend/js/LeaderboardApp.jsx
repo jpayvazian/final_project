5fcea9e5a2aace096a4095c6228d46ed912cd565
theaniartist <wsly@wpi.edu>
2021-10-04 15:02:58 -0400

    Add Leaderboard button and table setup

--- frontend/js/LeaderboardApp.jsx
+++ frontend/js/LeaderboardApp.jsx
@@ -4,6 +4,14 @@ const LeaderboardApp = () => {
 
     return (
         <div className="LeaderboardApp">
+            <h1>Leaderboard</h1>
+            <table>
+                <tr>
+                    <th scope="col">Username</th>
+                    <th scope="col"># of Games Played</th>
+                    <th scope="col">High Score</th>
+                </tr>
+            </table>
         </div>
     )
 }
