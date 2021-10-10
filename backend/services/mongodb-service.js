950ae4176d3572d07285fcb72eb3deb9a32584e6
Jack Ayvazian <65471365+jpayvazian@users.noreply.github.com>
2021-10-10 13:03:33 -0400

    unused room db collection

--- backend/services/mongodb-service.js
+++ backend/services/mongodb-service.js
@@ -3,21 +3,14 @@ const uri = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@${process
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let collectionUsers= null
-// let collectionRooms = null
 
 const connectDB = async function() {
     await client.connect()
     console.log('Successfully connected to MemoryDB')
     collectionUsers = client.db("MemoryDB").collection("Users");
-    // collectionRooms = client.db("MemoryDB").collection("Rooms");
 }
 
 connectDB()
-
-// exports.getUser = async function(id){
-//     const db_queryresults = await collectionUsers.findOne({ _id : ObjectId(id) })
-//     return db_queryresults
-// }
 
 exports.getGitHubUser = async function(id) {
   return new Promise((resolve, reject) => {
@@ -61,35 +54,3 @@ exports.getLeaderboard = async function(){
     const db_queryresults = await collectionUsers.find({ }).sort({"highscore": -1}).toArray()
     return db_queryresults
 }
-/*
-exports.createRoom = async function(key){
-    const db_queryresults = await collectionRooms.insertOne({ roomkey: key, players:[] }).toArray()
-    return db_queryresults
-}
-
-exports.updateRoomPlayers = async function(data){
-    const db_queryresults = await collectionRooms.updateOne({ _id : ObjectId(data.id)}, {
-        $push: {
-          players: {
-              playerName: data.name,
-          }
-        }  
-      })
-      return db_queryresults
-}
-
-exports.updateRoomScores = async function(data){
-    const db_queryresults = await collectionRooms.updateOne({ _id : ObjectId(data.id), "players.playerName" : data.name}, {
-        $set: {
-          "players.playerScore": data.score
-        } 
-      })
-      return db_queryresults
-}
-
-exports.deleteRoom = async function(data){
-    const db_queryresults = await collectionRooms.deleteOne({ _id: ObjectId(data.id) }).toArray()
-    return db_queryresults
-}
-*/
-
